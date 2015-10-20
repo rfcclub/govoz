b8d4c80cc11d22512fb46c41c245347a50c0a931
Nam Tu <rfcclub2011@gmail.com>
2015-10-20 16:20:09 +0700

    Remove htmlview project

--- goVoz/src/main/java/com/gotako/govoz/ThreadActivity.java
+++ goVoz/src/main/java/com/gotako/govoz/ThreadActivity.java
@@ -14,7 +14,6 @@ import java.util.concurrent.ExecutionException;
 
 import org.apache.http.impl.client.HttpClients;
 import org.jsoup.nodes.Document;
-import org.kobjects.htmlview.HtmlView;
 import org.w3c.dom.Text;
 
 import android.annotation.SuppressLint;
